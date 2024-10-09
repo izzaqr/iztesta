8cca1e3f3eb5852bf85c63bde83f426c451380e1
izzaqr <137944398+izzaqr@users.noreply.github.com>
2024-10-09 23:16:15 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 4.2,
+  spaceBetween: 30,
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
+});
+
+// ====================================================
+
+
